2ad611c54b5d935dc72d4505a124eee543c5924c
unknown <reuben.morgan@WKS-VIAT-08.Maingroup.Loc>
2013-10-01 14:19:38 +0100

    Initial commit, not yet tested

---
+++ LPC11U36-Blinky.c
@@ -0,0 +1,34 @@
+#include "LPC11Uxx.h"
+
+volatile uint32_t msTicks = 0; // counter for 1ms SysTicks
+
+//====================================================================================
+void main()
+{
+	uint32_t timer_mark;
+
+	// Enable GPIO Clock ( powers the GPIO peripheral )
+	LPC_SYSCON->SYSAHBCLKCTRL |= (1<<6);
+
+	// Select GPIO Mode and disable analog mode, refer to User Manual - UM10524
+	LPC_IOCON->PIO0_7 = 0;
+
+	// Set the pin direction, set high for an output
+	LPC_GPIO->DIR[0] |= 1<<7;
+	
+	// Init SysTick
+	SysTick_Config(SystemCoreClock / 1000);				// Generate interrupt every 1 ms
+	
+	for(;;)
+	{
+		timer_mark = msTicks;					// Take timer snapshot 
+		while(msTicks < (timer_mark + 100));	// Wait until 100ms has passed
+		LPC_GPIO->NOT[0] = 1<<7;
+	}
+}
+
+//====================================================================================
+void SysTick_Handler(void)
+{
+	msTicks++;
+}
